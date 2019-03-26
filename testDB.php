b8c53380d241d58ac426ddd67c07e1c01df02dc0
Esmir Osmanovic <46793150+Oesmir32@users.noreply.github.com>
2019-03-25 20:46:11 -0500

    Update testDB.php

--- testDB.php
+++ testDB.php
@@ -1,8 +1,8 @@
  <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
+$username = "CS485";
+$password = "WinonaState";
+$dbname = "CS485";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
